369ecd1e18106b475a3526eb6e1da448754c950e
xiaoxiongxiongxiong <981303787@qq.com>
2025-11-19 22:14:38 +0800

    修改项目框架

--- include/log.h
+++ include/log.h
@@ -7,21 +7,21 @@
 #include <inttypes.h>
 
 #ifdef __cplusplus
-#define LOG_API_BEGIN extern "C" {
-#define LOG_API_END }
+#define OS_LOG_API_BEGIN extern "C" {
+#define OS_LOG_API_END }
 #else
-#define LOG_API_BEGIN
-#define LOG_API_END
+#define OS_LOG_API_BEGIN
+#define OS_LOG_API_END
 #endif
 
 #if defined(WIN32) || defined(_WIN32)
-#if defined(LOG_API_EXPORT)
-#define LOG_API __declspec(dllexport)
+#if defined(OS_LOG_API_EXPORT)
+#define OS_LOG_API __declspec(dllexport)
 #else
-#define LOG_API __declspec(dllimport)
+#define OS_LOG_API __declspec(dllimport)
 #endif
 #else
-#define LOG_API
+#define OS_LOG_API
 #endif
 
 typedef enum _LOG_MSG_LEVEL  // 日志级别
@@ -33,7 +33,7 @@ typedef enum _LOG_MSG_LEVEL  // 日志级别
     LOG_LEVEL_FATAL          // 程序无法继续运行直接退出
 } LOG_MSG_LEVEL;
 
-LOG_API_BEGIN
+OS_LOG_API_BEGIN
 
 /*
 * log_msg_init
@@ -42,27 +42,27 @@ LOG_API_BEGIN
 * @param  level   写入文件的日志级别
 * @return 0--成功 -1--失败
 */
-LOG_API int log_msg_init(const char * file, LOG_MSG_LEVEL level);
+OS_LOG_API int log_msg_init(const char * file, LOG_MSG_LEVEL level);
 
 /*
 * log_msg_uninit
 * @brief  日志销毁
 */
-LOG_API void log_msg_uninit(void);
+OS_LOG_API void log_msg_uninit(void);
 
 /*
 * log_msg_set_slice_duration
 * @brief  设置日志切片时长，单位：毫秒(ms)
 * @param  ms 切片时长
 */
-LOG_API void log_msg_set_slice_duration(size_t ms);
+OS_LOG_API void log_msg_set_slice_duration(size_t ms);
 
 /*
 * log_msg_set_slice_size
 * @brief  设置日志切片大小
 * @param  kb 大小
 */
-LOG_API void log_msg_set_slice_size(size_t kb);
+OS_LOG_API void log_msg_set_slice_size(size_t kb);
 
 /*
 * log_msg
@@ -73,7 +73,7 @@ LOG_API void log_msg_set_slice_size(size_t kb);
 * @param  func   日志消息所在函数
 * @param  fmt    日志格式
 */
-LOG_API int log_msg(LOG_MSG_LEVEL level, const char * file, int line, const char * func, const char * fmt, ...);
+OS_LOG_API int log_msg(LOG_MSG_LEVEL level, const char * file, int line, const char * func, const char * fmt, ...);
 
 /*
 * log_msg_doit
@@ -85,7 +85,7 @@ LOG_API int log_msg(LOG_MSG_LEVEL level, const char * file, int line, const char
 * @param  fmt    日志格式
 * @param  vl     日志参数表
 */
-LOG_API int log_msg_doit(LOG_MSG_LEVEL level, const char * file, int line, const char * func, const char * fmt, va_list vl);
+OS_LOG_API int log_msg_doit(LOG_MSG_LEVEL level, const char * file, int line, const char * func, const char * fmt, va_list vl);
 
 #define log_msg_debug(...) log_msg(LOG_LEVEL_DEBUG, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
 #define log_msg_info(...) log_msg(LOG_LEVEL_INFO, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
@@ -93,6 +93,6 @@ LOG_API int log_msg_doit(LOG_MSG_LEVEL level, const char * file, int line, const
 #define log_msg_error(...) log_msg(LOG_LEVEL_ERROR, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
 #define log_msg_fatal(...) log_msg(LOG_LEVEL_FATAL, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
 
-LOG_API_END
+OS_LOG_API_END
 
 #endif
